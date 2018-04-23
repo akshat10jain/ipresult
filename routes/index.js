f110b1f7b78bd6987a3ee9b4fbb994d6ca964c91
yashgandhi-32 <yashgandhi32@gmail.com>
2018-04-23 20:28:11 +0530

    added

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb').MongoClient;
+
+/* GET home page. */
+router.get('/home', function (req, res, next) {
+  mongodb.connect('mongodb://admin:1234@ds253879.mlab.com:53879/students_record', function (err, db) {
+    var collection = db.collection('pdf_students_collection')
+    collection.findOne({ 'EnrollmentNo': '05276803115' }, function (err, result) {
+      if (result) {
+        res.render('index', { title: result })
+      }
+      if (err) {
+        console.log(err)
+      }
+    })
+  })
+});
+
+module.exports = router;
