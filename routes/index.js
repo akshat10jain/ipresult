ef6b0d4919b8be871e229391909e5d4c81e095bd
Akshat jain <akshat.jain100896@gmail.com>
2018-04-26 02:06:11 +0530

    userfile added

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var moment = require('moment');
-var mongodb = require('mongodb').MongoClient;
 
 /* GET home page. */
-router.get('/home', function (req, res, next) {
-  mongodb.connect('mongodb://admin:1234@ds253879.mlab.com:53879/students_record', function (err, db) {
-    var collection = db.collection('pdf_students_collection')
-    collection.findOne({ 'EnrollmentNo': '05276803115' }, function (err, result) {
-      if (result) {
-        res.render('index', { title: result , moment: moment })
-      }
-      if (err) {
-        console.log(err)
-      }
-    })
-  })
+router.get('/', function (req, res, next) {
+  res.render('index',{moment: moment})
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  mongodb.connect('mongodb://admin:1234@ds253879.mlab.com:53879/students_record', function (err, db) {
+    var collection = db.collection('pdf_students_collection')
+    collection.findOne({ 'EnrollmentNo': '05276803115' }, function (err, result) {
+      if (result) {
+        res.render('users', { title: result })
+      }
+      if (err) {
+        console.log(err)
+      }
+    })
+  })
 });
-
 module.exports = router;
+
